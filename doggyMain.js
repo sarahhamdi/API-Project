8c32f6bd4dd41cacc483a44cd639bafcba05f967
Sarah Hamdi <s.hamdi@gmail.com>
2016-02-23 17:50:35 -0500

    Added pseudo code to js file

--- doggyMain.js
+++ doggyMain.js
@@ -27,3 +27,22 @@ $(document).ready(function() {
 });
 
 
+
+// get user information (location + breeds)
+// on submit, push info to Petfinder to find dogs (requires location field to work)
+// get results from Petfinder on dogs
+// see exclusions below
+// get postal code from dogs
+// plot location of dogs on google maps based on postal code
+// display Pic, Name, Location, Description of dog
+
+// REQUIRED: 
+// key, location, animal, format (json), age (senior)
+
+// EXCLUDE (in an if/else statement):
+// if no zip code (undefined)
+// if animal is adopted (regex /adopted/ in 'petfinder.pets.pet[name]' field)
+
+
+// MOBILE EXTRAS
+// slide scroll on dynamic content
