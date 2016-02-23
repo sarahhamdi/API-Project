67b52c100472b06021dc49ee94df53ac402b4de6
Colin McCullough <colin.mccullo@gmail.com>
2016-02-23 11:49:56 -0500

    created js file and loaded JQuery to index.html

---
+++ doggyMain.js
@@ -0,0 +1,15 @@
+doggy.doggyAjax.apiKey = {
+	Colin: "290f422c91ecdc030991bbc422712f64"
+	//we can add our individual keys in here if you like 
+};
+
+var doggy = {
+};
+
+petfinderAPI.url = "****need to find the petfinderAPI URL****"
+
+// doggy.doggyAjax = function() {
+// 	$ajax.({
+
+// 	})
+// };
