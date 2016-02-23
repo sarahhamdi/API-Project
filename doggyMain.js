4bc999f7b17ee8c914e49b74855a540bb9af3dc7
Olivia Choy <olivia.choy2@gmail.com>
2016-02-23 17:50:40 -0500

    added code for finding persons location

--- doggyMain.js
+++ doggyMain.js
@@ -6,6 +6,33 @@ doggy.apiToken ="8463c41dbe3965fc6b42c2794511969d"
 doggy.doggyUrl = "http://api.petfinder.com/pet.find"
 
 
+
+doggy.getCurrentLocation = function() {
+		$.ajax({
+			url: "https://maps.googleapis.com/maps/api/geocode/json",
+			method: 'GET',
+			dataType: 'json',
+			data: {
+				address: 'L4J5X4'
+			}
+		}).then(function(result){
+			var lat = (result.results[0].geometry.location.lat);
+			var lng= (result.results[0].geometry.location.lng);
+			var latLng = lat + "," + lng;
+			console.log(latLng)
+
+			// doggy.myLatLng = {lat: lat, lng: lng}
+			
+			// var marker = new google.maps.Marker ({
+			// 	position: searchApp.myLatLng,
+			// 	map: searchApp.map,
+			// 	title: "You Are Here!"
+				
+			// })
+		});
+	}
+
+
 doggy.doggyAjax = function() {
 	$.ajax({
 		url: doggy.doggyUrl,
@@ -24,6 +51,7 @@ doggy.doggyAjax = function() {
 
 $(document).ready(function() {
 	doggy.doggyAjax();
+	doggy.getCurrentLocation();
 });
 
 
