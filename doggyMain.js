28aa441d801cf5173621728486c522ad5455b495
Olivia Choy <olivia.choy2@gmail.com>
2016-02-23 17:15:20 -0500

    added ajay for petfinder

--- doggyMain.js
+++ doggyMain.js
@@ -1,19 +1,29 @@
-doggy.doggyAjax.apiKey = {
-	Colin: "290f422c91ecdc030991bbc422712f64"
+var doggy = {};
+
+doggy.apiKey ="290f422c91ecdc030991bbc422712f64"
+doggy.apiToken ="8463c41dbe3965fc6b42c2794511969d"
 	//we can add our individual keys in here if you like 
+doggy.doggyUrl = "http://api.petfinder.com/pet.find"
+
+
+doggy.doggyAjax = function() {
+	$.ajax({
+		url: doggy.doggyUrl,
+		method: 'GET',
+		dataType: 'jsonp',
+		data : {
+			key: doggy.apiKey,
+			location: 'Toronto, On',
+			animal: 'dog',
+			format: 'json'
+		}  
+	}).then(function(results){
+		console.log(results);
+	});
 };
-
-var doggy = {
-};
-
-petfinderAPI.url = "****need to find the petfinderAPI URL****"
-
-// doggy.doggyAjax = function() {
-// 	$ajax.({
-
-// 	})
-// };
 
 $(document).ready(function() {
-	
+	doggy.doggyAjax();
 });
+
+
